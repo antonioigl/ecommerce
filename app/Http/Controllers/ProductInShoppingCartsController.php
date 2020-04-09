fc9a24b3922bf8af694b642aa1e0029bfcfdd6d5
Antonio <antonio241710@gmail.com>
2020-04-09 18:01:20 +0200

    rutas rest con opciones

---
+++ app/Http/Controllers/ProductInShoppingCartsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductInShoppingCartsController extends Controller
+{
+    public function store(Request $request)
+    {
+
+    }
+
+    public function delete(Request $request)
+    {
+
+    }
+}
